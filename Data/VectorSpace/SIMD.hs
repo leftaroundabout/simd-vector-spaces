7563013fabc743fa413a835eaad05136a32b44b6
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-04-21 02:06:31 +0200

    The important small vectors.

--- Data/VectorSpace/SIMD.hs
+++ Data/VectorSpace/SIMD.hs
@@ -29,6 +29,8 @@ import qualified Data.Primitive.SIMD as SIMD
 
 type ℝ = Double
 newtype ℝ² = ℝ² SIMD.DoubleX2
+newtype ℝ³ = ℝ³ SIMD.DoubleX4
+newtype ℝ⁴ = ℝ⁴ SIMD.DoubleX4
 
 #define SIMDVSInstances(V,F)       \
 instance AdditiveGroup (V) where {  \
@@ -40,6 +42,8 @@ instance VectorSpace V where {          \
   μ *^ V a = V $ SIMD.broadcastVector μ * a }
 
 SIMDVSInstances(ℝ², ℝ)
+SIMDVSInstances(ℝ³, ℝ)
+SIMDVSInstances(ℝ⁴, ℝ)
 
 
 data DirectSum v w = DirectSum !v !w
@@ -47,6 +51,11 @@ data DirectSum v w = DirectSum !v !w
 infixl 6 ⊕
 type family (⊕) v w where
   ℝ ⊕ ℝ = ℝ²
+  ℝ ⊕ ℝ² = ℝ³
+  ℝ² ⊕ ℝ = ℝ³
+  ℝ ⊕ ℝ³ = ℝ⁴
+  ℝ³ ⊕ ℝ = ℝ⁴
+  ℝ² ⊕ ℝ² = ℝ⁴
   v ⊕ w = DirectSum v w
 
 
@@ -64,4 +73,37 @@ instance KnownDirectSum ℝ ℝ where
   {-# INLINE split #-}
   split (ℝ² a) = SIMD.unpackVector a
 
+instance KnownDirectSum ℝ ℝ² where
+  {-# INLINE (⊕) #-}
+  (⊕) x (ℝ² yz) = ℝ³ $ SIMD.packVector (0,x,y,z) where (y,z) = SIMD.unpackVector yz
+  {-# INLINE split #-}
+  split (ℝ³ a) = (x, ℝ² $ SIMD.packVector (y,z)) where (_,x,y,z) = SIMD.unpackVector a
+
+instance KnownDirectSum ℝ² ℝ where
+  {-# INLINE (⊕) #-}
+  (⊕) (ℝ² xy) z = ℝ³ $ SIMD.packVector (0,x,y,z) where (x,y) = SIMD.unpackVector xy
+  {-# INLINE split #-}
+  split (ℝ³ a) = (ℝ² $ SIMD.packVector (x,y), z) where (_,x,y,z) = SIMD.unpackVector a
+
+instance KnownDirectSum ℝ ℝ³ where
+  {-# INLINE (⊕) #-}
+  (⊕) x (ℝ³ yzw) = ℝ⁴ $ SIMD.packVector (x,y,z,w) where (_,y,z,w) = SIMD.unpackVector yzw
+  {-# INLINE split #-}
+  split (ℝ⁴ a) = (x, ℝ³ $ SIMD.packVector (0,y,z,w)) where (x,y,z,w) = SIMD.unpackVector a
+
+instance KnownDirectSum ℝ³ ℝ where
+  {-# INLINE (⊕) #-}
+  (⊕) (ℝ³ wxy) z = ℝ⁴ $ SIMD.packVector (w,x,y,z) where (_,w,x,y) = SIMD.unpackVector wxy
+  {-# INLINE split #-}
+  split (ℝ⁴ a) = (ℝ³ $ SIMD.packVector (0,w,x,y), z) where (w,x,y,z) = SIMD.unpackVector a
+
+instance KnownDirectSum ℝ² ℝ² where
+  {-# INLINE (⊕) #-}
+  (⊕) (ℝ² xy) (ℝ² zw) = ℝ⁴ $ SIMD.packVector (x,y,z,w)
+   where (x,y) = SIMD.unpackVector xy; (z,w) = SIMD.unpackVector zw
+  {-# INLINE split #-}
+  split (ℝ⁴ a) = (ℝ² $ SIMD.packVector (x,y), ℝ² $ SIMD.packVector (z,w))
+   where (x,y,z,w) = SIMD.unpackVector a
+
+instance KnownDirectSum (DirectSum u v) w
 
