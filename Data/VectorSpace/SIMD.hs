71d55e0e088b7ca8b280a130426451c041cfd8f5
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-04-22 18:13:22 +0200

    Rename newtype constructors to ASCII R.
    Reason: why would prefer to use ℝ³ etc. as a better-usable, high-level
    pattern synonym.

--- Data/VectorSpace/SIMD.hs
+++ Data/VectorSpace/SIMD.hs
@@ -28,22 +28,22 @@ import qualified Data.Primitive.SIMD as SIMD
 
 
 type ℝ = Double
-newtype ℝ² = ℝ² SIMD.DoubleX2
-newtype ℝ³ = ℝ³ SIMD.DoubleX4
-newtype ℝ⁴ = ℝ⁴ SIMD.DoubleX4
+newtype ℝ² = R² SIMD.DoubleX2
+newtype ℝ³ = R³ SIMD.DoubleX4
+newtype ℝ⁴ = R⁴ SIMD.DoubleX4
 
-#define SIMDVSInstances(V,F)       \
+#define SIMDVSInstances(V,C,F)     \
 instance AdditiveGroup (V) where {  \
-  V a ^+^ V b = V (a+b);             \
-  zeroV = V 0;                        \
-  negateV (V a) = V $ negate a };      \
+  C a ^+^ C b = C (a+b);             \
+  zeroV = C 0;                        \
+  negateV (C a) = C $ negate a };      \
 instance VectorSpace V where {          \
   type Scalar (V) = (F);                 \
-  μ *^ V a = V $ SIMD.broadcastVector μ * a }
+  μ *^ C a = C $ SIMD.broadcastVector μ * a }
 
-SIMDVSInstances(ℝ², ℝ)
-SIMDVSInstances(ℝ³, ℝ)
-SIMDVSInstances(ℝ⁴, ℝ)
+SIMDVSInstances(ℝ², R², ℝ)
+SIMDVSInstances(ℝ³, R³, ℝ)
+SIMDVSInstances(ℝ⁴, R⁴, ℝ)
 
 
 data DirectSum v w = DirectSum !v !w
@@ -69,40 +69,40 @@ class KnownDirectSum v w where
 
 instance KnownDirectSum ℝ ℝ where
   {-# INLINE (⊕) #-}
-  (⊕) x y = ℝ² $ SIMD.packVector (x,y)
+  (⊕) x y = R² $ SIMD.packVector (x,y)
   {-# INLINE split #-}
-  split (ℝ² a) = SIMD.unpackVector a
+  split (R² a) = SIMD.unpackVector a
 
 instance KnownDirectSum ℝ ℝ² where
   {-# INLINE (⊕) #-}
-  (⊕) x (ℝ² yz) = ℝ³ $ SIMD.packVector (0,x,y,z) where (y,z) = SIMD.unpackVector yz
+  (⊕) x (R² yz) = R³ $ SIMD.packVector (0,x,y,z) where (y,z) = SIMD.unpackVector yz
   {-# INLINE split #-}
-  split (ℝ³ a) = (x, ℝ² $ SIMD.packVector (y,z)) where (_,x,y,z) = SIMD.unpackVector a
+  split (R³ a) = (x, R² $ SIMD.packVector (y,z)) where (_,x,y,z) = SIMD.unpackVector a
 
 instance KnownDirectSum ℝ² ℝ where
   {-# INLINE (⊕) #-}
-  (⊕) (ℝ² xy) z = ℝ³ $ SIMD.packVector (0,x,y,z) where (x,y) = SIMD.unpackVector xy
+  (⊕) (R² xy) z = R³ $ SIMD.packVector (0,x,y,z) where (x,y) = SIMD.unpackVector xy
   {-# INLINE split #-}
-  split (ℝ³ a) = (ℝ² $ SIMD.packVector (x,y), z) where (_,x,y,z) = SIMD.unpackVector a
+  split (R³ a) = (R² $ SIMD.packVector (x,y), z) where (_,x,y,z) = SIMD.unpackVector a
 
 instance KnownDirectSum ℝ ℝ³ where
   {-# INLINE (⊕) #-}
-  (⊕) x (ℝ³ yzw) = ℝ⁴ $ SIMD.packVector (x,y,z,w) where (_,y,z,w) = SIMD.unpackVector yzw
+  (⊕) x (R³ yzw) = R⁴ $ SIMD.packVector (x,y,z,w) where (_,y,z,w) = SIMD.unpackVector yzw
   {-# INLINE split #-}
-  split (ℝ⁴ a) = (x, ℝ³ $ SIMD.packVector (0,y,z,w)) where (x,y,z,w) = SIMD.unpackVector a
+  split (R⁴ a) = (x, R³ $ SIMD.packVector (0,y,z,w)) where (x,y,z,w) = SIMD.unpackVector a
 
 instance KnownDirectSum ℝ³ ℝ where
   {-# INLINE (⊕) #-}
-  (⊕) (ℝ³ wxy) z = ℝ⁴ $ SIMD.packVector (w,x,y,z) where (_,w,x,y) = SIMD.unpackVector wxy
+  (⊕) (R³ wxy) z = R⁴ $ SIMD.packVector (w,x,y,z) where (_,w,x,y) = SIMD.unpackVector wxy
   {-# INLINE split #-}
-  split (ℝ⁴ a) = (ℝ³ $ SIMD.packVector (0,w,x,y), z) where (w,x,y,z) = SIMD.unpackVector a
+  split (R⁴ a) = (R³ $ SIMD.packVector (0,w,x,y), z) where (w,x,y,z) = SIMD.unpackVector a
 
 instance KnownDirectSum ℝ² ℝ² where
   {-# INLINE (⊕) #-}
-  (⊕) (ℝ² xy) (ℝ² zw) = ℝ⁴ $ SIMD.packVector (x,y,z,w)
+  (⊕) (R² xy) (R² zw) = R⁴ $ SIMD.packVector (x,y,z,w)
    where (x,y) = SIMD.unpackVector xy; (z,w) = SIMD.unpackVector zw
   {-# INLINE split #-}
-  split (ℝ⁴ a) = (ℝ² $ SIMD.packVector (x,y), ℝ² $ SIMD.packVector (z,w))
+  split (R⁴ a) = (R² $ SIMD.packVector (x,y), R² $ SIMD.packVector (z,w))
    where (x,y,z,w) = SIMD.unpackVector a
 
 instance KnownDirectSum (DirectSum u v) w
